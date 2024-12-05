5ab5c890e961f72bceef9ad1d507a2cebad56b63
rachelelking <94739370+rachelelking@users.noreply.github.com>
2024-12-06 07:56:01 +1000

    Add files via upload

---
+++ src/app/user/user.component.ts
@@ -0,0 +1,35 @@
+import { Component, Input, Output, EventEmitter, output } from '@angular/core';
+import { type User } from './user.model';
+
+@Component({
+  selector: 'app-user',
+  standalone: true,
+  imports: [],
+  templateUrl: './user.component.html',
+  styleUrl: './user.component.css',
+})
+export class UserComponent {
+  @Input({ required: true }) user!: User;
+  @Input({ required: true }) selected!: boolean;
+
+  // @Input({ required: true }) id!: string;
+  // @Input({ required: true }) avatar!: string;
+  // @Input({ required: true }) name!: string;
+
+  @Output() select = new EventEmitter<string>();
+  // select = output<string>();
+
+  // avatar = input.required<string>();
+  // name = input.required<string>();
+
+  // imagePath = computed(() => {
+  //   return './assets/users/' + this.avatar();
+  // });
+  get imagePath() {
+    return 'assets/users/' + this.user.avatar;
+  }
+
+  onSelectUser() {
+    this.select.emit(this.user.id);
+  }
+}
